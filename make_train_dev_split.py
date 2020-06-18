85cded56a778622ee3a20ed7b3f476aa7e199a68
Gabriel Bernier-Colborne <g.b.colborne@gmail.com>
2020-06-18 19:49:25 -0400

    Major changes to functionality. See diff. I can't be bothered.

--- make_train_dev_split.py
+++ make_train_dev_split.py
@@ -3,13 +3,15 @@
 import os, argparse, random, logging
 import numpy as np
 from comp_utils import map_ULI_langs_to_paths, stream_sents, RELEVANT_LANGS
-
-REL_SUBSET_SIZE = 1000  # Nb sets in the relevant portion of the dev set
-IRR_SUBSET_SIZE = 1000  # Nb sets in the irrelevant portion of the dev set
+from iteround import saferound
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--balance-irrelevant", "-b", action="store_true",
+parser.add_argument("--balance-irrelevant", action="store_true",
                     help="Balance irrelevant languages.")
+parser.add_argument("--rel_size", type=int, default=6000,
+                    help="Number of relevant sentences in dev set")
+parser.add_argument("--irr_size", type=int, default=4000,
+                    help="Number of irrelevant sentences in dev set")
 parser.add_argument("output_dir")
 args = parser.parse_args()
 
@@ -25,7 +27,6 @@ logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(messa
                     datefmt = '%m/%d/%Y %H:%M:%S',
                     level = logging.DEBUG)
 
-
 # Seed RNG
 random.seed(91500)
 
@@ -38,65 +39,65 @@ irr_langs = sorted(set(langs).difference(rel_langs))
 rel_lang2id = dict((l,i) for (i,l) in enumerate(rel_langs))
 irr_lang2id = dict((l,i) for (i,l) in enumerate(irr_langs))
 
-# Compute class frequencies
+# Compute expected distribution of dev set based on training data
+logger.info("Computing expected distribution of dev set based on training data...")
 lang2freq = {}
-logger.info("Computing class frequencies...")
 for lang in langs:
     logger.info("  " + lang)
     lang2freq[lang] = sum(1 for (sent, url) in stream_sents(lang))
-
-# Compute sampling probabilities (separately for each of the 2 groups)
-rel_counts = np.array([lang2freq[k] for k in rel_langs], dtype=np.float32)
+rel_counts = np.array([lang2freq[k] for k in rel_langs], dtype=np.float)
 rel_probs = rel_counts / rel_counts.sum()
 if args.balance_irrelevant:
-    irr_probs = np.ones(len(irr_langs), dtype=np.float32) / len(irr_langs)
+    irr_probs = np.ones(len(irr_langs), dtype=float) / len(irr_langs)
 else:
-    irr_counts = np.array([lang2freq[k] for k in irr_langs], dtype=np.float32)
+    irr_counts = np.array([lang2freq[k] for k in irr_langs], dtype=np.float)
     irr_probs = irr_counts / irr_counts.sum()
-for subsize in [500, 1000, 2500, 5000, 10000, 25000, 50000]:
-    rel_cum_probs = rel_probs * subsize
-    print(rel_cum_probs)
-    irr_cum_probs = irr_probs * subsize
-    print(irr_cum_probs)
-    rel_excluded = (rel_cum_probs < 0.5).sum()
-    irr_excluded = (irr_cum_probs < 0.5).sum()
-    print("{}\t{}\t{}".format(subsize, rel_excluded, irr_excluded))
-sys.exit()
+# Compute expected count of dev sentences. Use a sum-safe rounding function.
+rel_dev_counts = [int(x) for x in saferound(rel_probs * args.rel_size, 0, "difference")]
+irr_dev_counts = [int(x) for x in saferound(irr_probs * args.irr_size, 0, "difference")]
 
-# Make train-dev split
-lang2dev = dict([(lang,[]) for lang in langs])
-logger.info("Sampling dev set...")
-for i in range(MAX_SENTS):
-    # Sample a language
-    lang = random.choice(langs)
-    # Sample a sentence
-    sent_id = random.randint(0,lang2size[lang]-1)
-    lang2dev[lang].append(sent_id)
-        
-    # Now loop over data again and write split
-    data = []
-    logger.info("Writing train-dev split --> %s..." % args.output_dir)
-    path_train = os.path.join(args.output_dir, "train.tsv")
-    path_dev = os.path.join(args.output_dir, "valid.tsv")
-    f_train = open(path_train, 'w')
-    f_dev = open(path_dev, 'w')
-    for lang in langs:
-        logger.info("  " + lang)
-        dev_ids = sorted(lang2dev[lang])
-        for i,(sent, url) in enumerate(stream_sents(lang)):
-            # Remove any tabs from text, as data is written in tsv
-            sent = sent.replace("\t", " ")
-            # Write
-            example = "%s\t%s\n" % (sent,lang)
-            if len(dev_ids) and i == dev_ids[0]:
+def build_example(text, lang):
+    return "%s\t%s" % (text, lang)
+
+# Make train/dev split
+logger.info("Writing train-dev split --> %s..." % args.output_dir)
+path_train = os.path.join(args.output_dir, "train.tsv")
+path_dev = os.path.join(args.output_dir, "valid.tsv")
+f_train = open(path_train, 'w')
+f_dev = open(path_dev, 'w')
+logger.info("  ----------------")
+logger.info("  lang (train/dev)")
+logger.info("  ----------------")
+for lang in langs:
+    nb_sents = int(lang2freq[lang])
+    all_indices = list(range(nb_sents))
+    if lang in rel_lang2id:
+        nb_dev = rel_dev_counts[rel_lang2id[lang]]
+    else:
+        nb_dev = irr_dev_counts[irr_lang2id[lang]]
+    nb_train = nb_sents - nb_dev
+    logger.info("  %s (%d/%d)" % (lang, nb_train, nb_dev))
+    dev_empty = nb_dev == 0
+    if not dev_empty:
+        dev_indices = np.random.randint(0, high=nb_sents, size=nb_dev)
+        sorted_dev_indices = sorted(dev_indices)
+    for i, (text,url) in enumerate(stream_sents(lang)):
+        example = build_example(text, lang)
+        example += "\n"
+        if dev_empty:
+            f_train.write(example)
+        else:
+            if i == sorted_dev_indices[0]:
                 f_dev.write(example)
-                dev_ids.pop(0)
+                _ = sorted_dev_indices.pop(0)
+                if len(sorted_dev_indices) == 0:
+                    dev_empty = True
             else:
                 f_train.write(example)
-    f_train.close()
-    f_dev.close()
+f_train.close()
+f_dev.close()
 
-    logger.warning(" !!! YOU SHOULD SHUFFLE THE DATASET, AS IT IS ORDERED BY LANG !!! ")
-    logger.warning(" !!! YOU SHOULD SHUFFLE THE DATASET, AS IT IS ORDERED BY LANG !!! ")
-    logger.warning(" !!! YOU SHOULD SHUFFLE THE DATASET, AS IT IS ORDERED BY LANG !!! ")    
+logger.warning(" !!! YOU SHOULD SHUFFLE THE DATASET, AS IT IS ORDERED BY LANG !!! ")
+logger.warning(" !!! YOU SHOULD SHUFFLE THE DATASET, AS IT IS ORDERED BY LANG !!! ")
+logger.warning(" !!! YOU SHOULD SHUFFLE THE DATASET, AS IT IS ORDERED BY LANG !!! ")    
                 
