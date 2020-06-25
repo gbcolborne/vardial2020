07e15f301ac21a2ceff3a9f5bc1ee44c1f566734
Gabriel Bernier-Colborne <g.b.colborne@gmail.com>
2020-06-25 10:38:49 -0400

    Implements tracks 1 and 3

--- scorer.py
+++ scorer.py
@@ -53,31 +53,52 @@ def main():
     print("Gold:")
     print(gold)
     print("\n\n")
-    
-    if args.track in [1,3]:
-        # For these 2 tracks, the score is the average (macro)
-        # f1-score over languages. For track 1, these are the 29
-        # relevant Uralic languages. For track 3, these are all 178
-        # languages.
-        if args.track == 1:
-            labels = sorted(rel_lang_ids)
-        elif args.track == 3:
-            labels = sorted(land2id.keys())
-        conf = mcm(gold, pred, sample_weight=None, labels=labels, samplewise=False)
-        for i in range(len(labels)):
-            print("\nLabel: %d" % labels[i])
-            print("Conf:")
-            print(conf[i])
-        
-    elif args.track == 2:
-        # For this track, the score is the micro-averaged f1-score
+    if args.track == 2:
+        # For track 2, the score is the micro-averaged f1-score
         # over sentences.
         p,r,f,_ = prfs(gold, pred, beta=1.0, labels=None, average="micro", zero_division=0)
         title = "Results (track 2)"
         print_title_with_border(title)
         print("Precision: {:.4f}".format(p))
         print("Recall: {:.4f}".format(r))
-        print("F-score: {:.4f}\n".format(f))        
+        print("F1-score: {:.4f}\n".format(f))
+    elif args.track in [1,3]:
+        # For tracks 1 and 3, the score is the average (macro)
+        # f1-score over a set of languages. For track 1, these are the
+        # 29 relevant Uralic languages. For track 3, these are all 178
+        # languages.
+        if args.track == 1:
+            labels = sorted(rel_lang_ids)
+        elif args.track == 3:
+            labels = sorted(land2id.keys())
+        conf = mcm(gold, pred, sample_weight=None, labels=labels, samplewise=False)
+        f1scores = []
+        for i in range(len(labels)):
+            tn = conf[i,0,0]
+            tp = conf[i,1,1]                        
+            fp = conf[i,0,1]
+            fn = conf[i,1,0]
+            nb_pred = tp + fp
+            nb_gold = tp + fn
+            precision = tp / nb_pred
+            recall = tp / nb_gold
+            f1score = 2 * precision * recall / (precision + recall)
+            f1scores.append(f1score)
+            print("\nLabel: %s" % all_langs[labels[i]])
+            print("  tn: %d" % tn)
+            print("  tp: %d" % tp)
+            print("  fp: %d" % fp)
+            print("  fn: %d" % fn)
+            print("  nb_pred: %d" % nb_pred)
+            print("  nb_gold: %d" % nb_gold)
+            print("  precision: %f" % precision)
+            print("  recall: %f" % recall)
+            print("  f1score: %f" % f1score)
+        macro_f1score = sum(f1scores) / len(f1scores)
+        title = "Results (track %d)" % args.track
+        print_title_with_border(title)
+        print("Average (macro) F1-score: {:.4f}".format(macro_f1score))
+            
 
         
 if __name__ == "__main__":
