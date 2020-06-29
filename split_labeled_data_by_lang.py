a51e3262321f9676a6c0bda855296a858d74f09c
Gabriel Bernier-Colborne <g.b.colborne@gmail.com>
2020-06-29 08:00:56 -0400

    Fixes various naming and syntax errors

--- split_labeled_data_by_lang.py
+++ split_labeled_data_by_lang.py
@@ -10,28 +10,28 @@ def main():
     args = parser.parse_args()
 
     # Check args
-    assert "train.tsv" in os.listdir(input_dir) and "valid.tsv" in os.listdir(input_dir)
+    assert "train.tsv" in os.listdir(args.input_dir) and "valid.tsv" in os.listdir(args.input_dir)
     if os.path.exists(args.output_dir):
         assert os.path.isdir(args.output_dir) and len(os.listdir(args.output_dir)) == 0
     else:
         os.makedirs(args.output_dir)
     parts = ["train", "valid"]
-    if "test.tsv" in os.listdir(input_dir):
+    if "test.tsv" in os.listdir(args.input_dir):
         parts.append("test")
 
     # Process data
     for part in parts:
-        lang2out = {lang:open("%s.%s" % (lang, part), 'w') for lang in ALL_LANGS}
-        fni = "%s.tsv" % part
+        lang2out = {lang:open(os.path.join(args.output_dir, "%s.%s" % (lang, part)), 'w') for lang in ALL_LANGS}
+        fni = os.path.join(args.input_dir, "%s.tsv" % part)
         with open(fni) as fi:
             for line in fi:
                 elems = line.strip().split("\t")
-                elems[0] = text
-                elems[1] = lang
+                text = elems[0] 
+                lang = elems[1] 
                 assert lang in ALL_LANGS
                 lang2out[lang].write(text + "\n")
         for outfile in lang2out.values():
             outfile.close()
     
-if __name__ == "__main__"():
+if __name__ == "__main__":
     main()
