505a1c2107997b7343bec9742f921dd4c5ab180b
Gabriel Bernier-Colborne <g.b.colborne@gmail.com>
2020-06-29 07:37:51 -0400

    Initial commit for split_labeled_data_by_lang.py

---
+++ split_labeled_data_by_lang.py
@@ -0,0 +1,37 @@
+""" Split labeled data by language. """
+
+import os, argparse
+from comp_utils import ALL_LANGS
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_dir", help="Path of directory containing files named train.tsv and valid.tsv, and optionally test.tsv")
+    parser.add_argument("output_dir")
+    args = parser.parse_args()
+
+    # Check args
+    assert "train.tsv" in os.listdir(input_dir) and "valid.tsv" in os.listdir(input_dir)
+    if os.path.exists(args.output_dir):
+        assert os.path.isdir(args.output_dir) and len(os.listdir(args.output_dir)) == 0
+    else:
+        os.makedirs(args.output_dir)
+    parts = ["train", "valid"]
+    if "test.tsv" in os.listdir(input_dir):
+        parts.append("test")
+
+    # Process data
+    for part in parts:
+        lang2out = {lang:open("%s.%s" % (lang, part), 'w') for lang in ALL_LANGS}
+        fni = "%s.tsv" % part
+        with open(fni) as fi:
+            for line in fi:
+                elems = line.strip().split("\t")
+                elems[0] = text
+                elems[1] = lang
+                assert lang in ALL_LANGS
+                lang2out[lang].write(text + "\n")
+        for outfile in lang2out.values():
+            outfile.close()
+    
+if __name__ == "__main__"():
+    main()
