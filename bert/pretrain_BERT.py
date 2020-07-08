b38dfee50b7f72e6c32f4fcabb40908eb543646b
Gabriel Bernier-Colborne <g.b.colborne@gmail.com>
2020-07-08 09:14:39 -0400

    Updates docstring, removes original license statement

--- bert/pretrain_BERT.py
+++ bert/pretrain_BERT.py
@@ -1,23 +1,5 @@
-# coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors and The HugginFace Inc. team.
-# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-Pre-train BERT.
-
-Code based on: https://github.com/huggingface/pytorch-pretrained-BERT/blob/master/examples/run_lm_finetuning.py.
+"""Pre-train BERT for language identification using masked language
+modeling and sequence pair classification.
 
 """
 
@@ -43,7 +25,7 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-    
+
 def count_params(model):
     count = 0
     for p in model.parameters():
