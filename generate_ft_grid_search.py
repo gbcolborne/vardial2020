df027d12340266bcc6cc290e00584fddad3e46c3
Gabriel Bernier-Colborne <g.b.colborne@gmail.com>
2020-07-16 14:39:12 -0400

    Adds hparams

--- generate_ft_grid_search.py
+++ generate_ft_grid_search.py
@@ -15,16 +15,18 @@ def main():
         raise RuntimeError(msg)
     if not os.path.exists(args.dir_output_jobs):
         os.makedirs(args.dir_output_jobs)
-        
+
+    # Tuned hparams
     settings = {}
     settings["train_batch_size"] = ["16", "32"]
     settings["seq_len"] = ["128", "256"]
     settings["learning_rate"] = ["2e-5", "3e-5", "5e-5"]
     settings["grad_accum_steps"] = ["1", "2"]
+    settings["sampling_distro"] = ["uniform", "relfreq", "dampfreq"]
     
     # Enumerate combinations
     combinations = [[]]
-    for k in ["train_batch_size", "seq_len", "learning_rate", "grad_accum_steps"]:
+    for k in ["train_batch_size", "seq_len", "learning_rate", "grad_accum_steps", "sampling_distro"]:
         new_combinations = []
         for c in combinations:
             for v in settings[k]:
@@ -34,10 +36,10 @@ def main():
     
     # Check combinations
     filtered = []
-    for (bs, sl, lr, gs) in combinations:
+    for (bs, sl, lr, gs, sd) in combinations:
         if bs == "32" and sl == "256":
             continue
-        filtered.append((bs, sl, lr, gs))
+        filtered.append((bs, sl, lr, gs, sd))
     combinations = filtered
     print("Nb combinations remaining after filtering: %d" % len(combinations))
 
@@ -46,9 +48,9 @@ def main():
         job_template = f.read().strip()
     
     # Write job files (assume working directory is vardial/bert, and both the pretrained model and the finetuned model are saved here)
-    for i, (bs, sl, lr, gs) in enumerate(combinations):
+    for i, (bs, sl, lr, gs, sd) in enumerate(combinations):
         # Encode settings in output directory path
-        dir_output = "Finetune_bs=%s_sl=%s_lr=%s_gs=%s" % (bs, sl, lr, gs)        
+        dir_output = "FT_exp2_bs=%s_sl=%s_lr=%s_gs=%s_sd=%s" % (bs, sl, lr, gs, sd)        
 
         # Build command
         cmd = "CUDA_VISIBLE_DEVICES=\"0\" python run_classifier.py"
@@ -56,10 +58,12 @@ def main():
         cmd += " --dir_data %s" % ("../data/finetuning_data_cut256" if sl == "256" else "../data/finetuning_data_cut128")
         cmd += " --do_train --eval_during_training"
         cmd += " --max_train_steps %d" % args.max_train_steps
+        cmd += " --sampling_distro %s" % sd
         cmd += " --train_batch_size %s" % bs
         cmd += " --seq_len %s" % sl
         cmd += " --grad_accum_steps %s" % gs
         cmd += " --learning_rate %s" % lr
+        cmd += " --avgpool"
         cmd += " --dir_output %s" % dir_output
 
         # Write
