c830802ff2f52a6def16ca63493a4021759c4848
Gabriel Bernier-Colborne <g.b.colborne@gmail.com>
2020-07-21 09:41:00 -0400

    Adds flag USE_BEST_SCORE

--- analyze_scores.py
+++ analyze_scores.py
@@ -8,11 +8,14 @@ parser.add_argument("dir_models")
 parser.add_argument("track", type=int, choices=[1,2,3])
 args = parser.parse_args()
 
+USE_BEST_SCORE = False # If True, a model's score is its best score
+                       # across epochs. Otherwise, we use the score at
+                       # the end of training.
+
 score_name = "DevF1Track" + str(args.track)
 model_dirs = os.listdir(args.dir_models)
 all_settings = []
-all_max_scores = []
-all_last_scores = []
+all_scores = []
 for d in model_dirs:
     # Parse directory name, which encodes hparam settings
     settings = {}
@@ -34,28 +37,25 @@ for d in model_dirs:
         for line in f:
             cols = line.strip().split("\t")
             scores.append(float(cols[score_col_ix]))
-    max_score = max(scores)
-    last_score = scores[-1]
+    if USE_BEST_SCORE:
+        score = max(scores)
+    else:
+        score = scores[-1]
     all_settings.append(settings)
-    all_max_scores.append(max_score)
-    all_last_scores.append(last_score)
+    all_scores.append(score)
+
 
 # Analyze scores wrt hparam settings
 hparam_names = list(all_settings[0].keys())
 for hname in hparam_names:
-    hval_to_max_scores = {}
-    hval_to_last_scores = {}
+    hval_to_scores = {}
     for i in range(len(all_settings)):
         hval = all_settings[i][hname]
-        if hval not in hval_to_max_scores:
-            hval_to_max_scores[hval] = []
-            hval_to_last_scores[hval] = []        
-        hval_to_max_scores[hval].append(all_max_scores[i])
-        hval_to_last_scores[hval].append(all_last_scores[i])
+        if hval not in hval_to_scores:
+            hval_to_scores[hval] = []
+        hval_to_scores[hval].append(all_scores[i])
     print("\nHyperparameter: %s" % hname)
-    for val in sorted(hval_to_max_scores.keys()):
+    for val in sorted(hval_to_scores.keys()):
         print("- %s" % val)
-        print("  - mean best score: %f" % np.mean(hval_to_max_scores[val])) 
-        print("  - max best score: %f" % max(hval_to_max_scores[val])) 
-        print("  - mean last score: %f" % np.mean(hval_to_last_scores[val])) 
-        print("  - max last score: %f" % max(hval_to_last_scores[val])) 
+        print("  - mean score: %f" % np.mean(hval_to_scores[val])) 
+        print("  - max score: %f" % max(hval_to_scores[val])) 
