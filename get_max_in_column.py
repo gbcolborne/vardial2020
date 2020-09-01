9f4b20365b40a9b7cbb5bcbc4d4df02748baa7ae
Gabriel Bernier-Colborne <g.b.colborne@gmail.com>
2020-09-01 10:38:08 -0400

    Initial commit for get_max_in_column.py

---
+++ get_max_in_column.py
@@ -0,0 +1,22 @@
+""" Get maximum value in column of a TSV file. """
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path_tsv", type=str)
+parser.add_argument("col_index", type=int)
+args = parser.parse_args()
+
+# Load data
+with open(args.path_tsv) as f:
+    data = []
+    for line in f:
+        elems = line.strip().split("\t")
+        data.append(elems[args.col_index])
+
+col_name = data[0]
+data = data[1:]
+data = [float(x) for x in data]
+print("Col name: %s" % col_name)
+print("First value: %f" % data[0])
+print("Max value: %f" % max(data))
